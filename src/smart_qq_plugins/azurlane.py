27cee4c77d2d1aaf3186ffeca26aacfb8f41e5c8
Punizione <1273435147@qq.com>
2018-10-23 14:55:48 +0800

    fix \
    update: readme
    
    simple commit
    
    fix pattern
    
    fix pattern
    
    fix selector
    
    fix soup
    
    six 拼写
    
    fix 拼写
    
    fix 拼写
    
    fix 拼写
    
    fix selector
    
    fix bugs

--- src/smart_qq_plugins/azurlane.py
+++ src/smart_qq_plugins/azurlane.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 import re
 
 import requests
@@ -7,69 +9,74 @@ from smart_qq_bot.signals import on_group_message
 from bs4 import BeautifulSoup
 
 # 类型
-SHIP_TYPE_SELECTOR = "table.wikitable.sv-general > tbody > tr:nth-child(3) > td:nth-child(2)"
+SHIP_TYPE_SELECTOR = "table.wikitable.sv-general > tbody > tr:nth-of-type(3) > td:nth-of-type(2)"
 
 #星级
-SHIP_LEVEL_SELECTOR = "table.wikitable.sv-general > tbody > tr:nth-child(3) > td:nth-child(4)"
+SHIP_LEVEL_SELECTOR = "table.wikitable.sv-general > tbody > tr:nth-of-type(3) > td:nth-of-type(4)"
 
 #建造时间
-SHIP_BUILDTIME_SELECTOR = "table.wikitable.sv-general > tbody > tr:nth-child(4) > td:nth-child(4)"
+SHIP_BUILDTIME_SELECTOR = "table.wikitable.sv-general > tbody > tr:nth-of-type(4) > td:nth-of-type(4)"
 
 #普通掉落点
-SHIP_DROP_NORMAL_SELECTOR = "table.wikitable.sv-general > tbody > tr:nth-child(5) > td:nth-child(2)"
+SHIP_DROP_NORMAL_SELECTOR = "table.wikitable.sv-general > tbody > tr:nth-of-type(5) > td:nth-of-type(2)"
 
 #特殊掉落点
-SHIP_DROP_SPECIAL_SELECTOR = "table.wikitable.sv-general > tbody > tr:nth-child(6) > td:nth-child(2)"
+SHIP_DROP_SPECIAL_SELECTOR = "table.wikitable.sv-general > tbody > tr:nth-of-type(6) > td:nth-of-type(2)"
 
 #耐久
-SHIP_ATTR_DURABLE_SELECTOR = "table.wikitable.sv-performance > tbody > tr:nth-child(4) > td:nth-child(2)"
+SHIP_ATTR_DURABLE_SELECTOR = "table.wikitable.sv-performance > tbody > tr:nth-of-type(4) > td:nth-of-type(2)"
 
 #炮击
-SHIP_ATTR_CANNONRY_SELECTOR = "table.wikitable.sv-performance > tbody > tr:nth-child(5) > td:nth-child(2)"
+SHIP_ATTR_CANNONRY_SELECTOR = "table.wikitable.sv-performance > tbody > tr:nth-of-type(5) > td:nth-of-type(2)"
 
 #防空
-SHIP_ATTR_AIR_DEFENSE_SELECTOR = "table.wikitable.sv-performance > tbody > tr:nth-child(6) > td:nth-child(2)"
+SHIP_ATTR_AIR_DEFENSE_SELECTOR = "table.wikitable.sv-performance > tbody > tr:nth-of-type(6) > td:nth-of-type(2)"
 
 #反潜
-SHIP_ATTR_ANTISUBMARINE_SELECTOR = "table.wikitable.sv-performance > tbody > tr:nth-child(7) > td:nth-child(2)"
+SHIP_ATTR_ANTISUBMARINE_SELECTOR = "table.wikitable.sv-performance > tbody > tr:nth-of-type(7) > td:nth-of-type(2)"
 
 #装甲
-SHIP_ATTR_AMMR_SELECTOR = "table.wikitable.sv-performance > tbody > tr:nth-child(4) > td:nth-child(4)"
+SHIP_ATTR_AMMR_SELECTOR = "table.wikitable.sv-performance > tbody > tr:nth-of-type(4) > td:nth-of-type(4)"
 
 #装填
-SHIP_ATTR_RELOAD_SELECTOR = " table.wikitable.sv-performance > tbody > tr:nth-child(4) > td:nth-child(6)"
+SHIP_ATTR_RELOAD_SELECTOR = " table.wikitable.sv-performance > tbody > tr:nth-of-type(4) > td:nth-of-type(6)"
 
 #雷击
-SHIP_ATTR_LIGHTNING_SELECTOR = "table.wikitable.sv-performance > tbody > tr:nth-child(5) > td:nth-child(4)"
+SHIP_ATTR_LIGHTNING_SELECTOR = "table.wikitable.sv-performance > tbody > tr:nth-of-type(5) > td:nth-of-type(4)"
 
 #机动
-SHIP_ATTR_MOTOR_SELECTOR = "table.wikitable.sv-performance > tbody > tr:nth-child(5) > td:nth-child(6)"
+SHIP_ATTR_MOTOR_SELECTOR = "table.wikitable.sv-performance > tbody > tr:nth-of-type(5) > td:nth-of-type(6)"
 
 #航空
-SHIP_ATTR_AVIATION_SELECTOR = "table.wikitable.sv-performance > tbody > tr:nth-child(6) > td:nth-child(4)"
+SHIP_ATTR_AVIATION_SELECTOR = "table.wikitable.sv-performance > tbody > tr:nth-of-type(6) > td:nth-of-type(4)"
 
 #油耗
-SHIP_ATTR_OIL_SELECTOR = "table.wikitable.sv-performance > tbody > tr:nth-child(6) > td:nth-child(6)"
+SHIP_ATTR_OIL_SELECTOR = "table.wikitable.sv-performance > tbody > tr:nth-of-type(6) > td:nth-of-type(6)"
 
 #航速
-SHIP_ATTR_SPEED_SELECTOR = "table.wikitable.sv-performance > tbody > tr:nth-child(8) > td:nth-child(2)"
+SHIP_ATTR_SPEED_SELECTOR = "table.wikitable.sv-performance > tbody > tr:nth-of-type(8) > td:nth-of-type(2)"
 
 #推荐PVE配装
-SHIP_EQUIPMENT_SELECTOR = "div.pverecommend1 .REt > span > a~a"
+SHIP_EQUIPMENT_SELECTOR = "div.pverecommend1 .REt > span > a:nth-of-type(2)"
 
+# 别称/花名
+nickname_dict = {
+	"克爹": "克利夫兰",
+	"": ""
+}
 @on_group_message
 def ship(msg, bot):
 	match1 = re.match(
 		six.text_type('^(船|老婆|舰娘) (.*)'),
 		msg.content
 	)
-	match2 = re.match(
-		six.text_type("^()")
-	)
+	# match2 = re.match(
+	# 	six.text_type("^(=)")
+	# )
 	if match1:
 		logger.info("RUNTIMELOG 查询舰娘...")
-		command = match.group(1)
-		ship = match.group(2)
+		command = match1.group(1)
+		ship = match1.group(2)
 		logger.info("RUNTIMELOG 查询舰娘语句: "+msg.content)
 		if command == six.text_type("船") or command == six.text_type("老婆") or command == six.text_type("舰娘"):
 			str_data = ""
@@ -80,11 +87,11 @@ def ship(msg, bot):
 @on_group_message
 def picture_stream(msg, bot):
 	match1 = re.match(
-		six.text_type("^(装备|装备一图流)\s?(井号|#)"),
+		six.text_type("^(装备|装备一图流)\s?(井号|#)?"),
 		msg.content
 	)
 	match2 = re.match(
-		six.text_type("^(天梯|舰娘排行)\s?(兔老师)"),
+		six.text_type("^(天梯|舰娘排行)\s?(兔老师)?"),
 		msg.content
 	)
 
@@ -96,23 +103,23 @@ def exctrace_ship(ship=None):
 	url = "http://wiki.joyme.com/blhx/{ship}".format(
 		ship=ship
 	)
-	reply_str = "类型:{ship_type}\n"+
-		"稀有度:{ship_level}\n"+
-		"建造时间:{ship_buildtime}\n"+
-		"普通掉落:{ship_drop_normal}\n"+
-		"特殊掉落:{ship_drop_special}\n"+
-		"耐久:{ship_attr_durable}\n"+
-		"炮击:{ship_attr_cannonry}\n"+
-		"防空:{ship_attr_air_defense}\n"+
-		"反潜:{ship_attr_antisubmarine}\n"+
-		"装甲:{ship_attr_ammr}\n"+
-		"装填:{ship_attr_reload}\n"+
-		"雷击:{ship_attr_lighting}\n"+
-		"机动:{ship_attr_motor}\n"+
-		"航空:{ship_attr_aviation}\n"+
-		"油耗:{ship_attr_oil}\n"+
-		"航速:{ship_attr_speed}\n"+
-		"配装:{ship_equip}\n"
+	reply_str = "类型:{ship_type}"+ \
+		"稀有度:{ship_level}"+ \
+		"建造时间:{ship_buildtime}"+ \
+		"普通掉落:{ship_drop_normal}"+ \
+		"特殊掉落:{ship_drop_special}"+ \
+		"耐久:{ship_attr_durable}"+ \
+		"炮击:{ship_attr_cannonry}"+ \
+		"防空:{ship_attr_air_defense}"+ \
+		"反潜:{ship_attr_antisubmarine}"+ \
+		"装甲:{ship_attr_ammr}"+ \
+		"装填:{ship_attr_reload}"+ \
+		"雷击:{ship_attr_lighting}"+ \
+		"机动:{ship_attr_motor}"+ \
+		"航空:{ship_attr_aviation}"+ \
+		"油耗:{ship_attr_oil}"+ \
+		"航速:{ship_attr_speed}"+ \
+		"配装:{ship_equip}"
 
 	try:
 		response = requests.request("GET", url)
@@ -121,6 +128,12 @@ def exctrace_ship(ship=None):
 		logger.error(ex)
 		str_data = "请求失败 :("
 		return str_data
+	match = re.match(
+		six.text_type("本页面目前没有内容"),
+		response.text
+	)
+	if match:
+		return "emmm这可能是颗卫星 :("
 	ship_type = extract_first_match(SHIP_TYPE_SELECTOR, soup)
 	ship_level = extract_first_match(SHIP_LEVEL_SELECTOR, soup)
 	ship_buildtime = extract_first_match(SHIP_BUILDTIME_SELECTOR, soup)
@@ -131,7 +144,7 @@ def exctrace_ship(ship=None):
 	ship_attr_air_defense = extract_first_match(SHIP_ATTR_AIR_DEFENSE_SELECTOR, soup)
 	ship_attr_antisubmarine = extract_first_match(SHIP_ATTR_ANTISUBMARINE_SELECTOR, soup)
 	ship_attr_ammr = extract_first_match(SHIP_ATTR_AMMR_SELECTOR, soup)
-	ship_atrr_reload = extract_first_match(SHIP_ATTR_RELOAD_SELECTOR, soup)
+	ship_attr_reload = extract_first_match(SHIP_ATTR_RELOAD_SELECTOR, soup)
 	ship_attr_lighting = extract_first_match(SHIP_ATTR_RELOAD_SELECTOR, soup)
 	ship_attr_motor = extract_first_match(SHIP_ATTR_RELOAD_SELECTOR, soup)
 	ship_attr_aviation = extract_first_match(SHIP_ATTR_RELOAD_SELECTOR, soup)
@@ -150,7 +163,7 @@ def exctrace_ship(ship=None):
 		ship_attr_air_defense=ship_attr_air_defense,
 		ship_attr_antisubmarine=ship_attr_antisubmarine,
 		ship_attr_ammr=ship_attr_ammr,
-		ship_atrr_reload=ship_atrr_reload,
+		ship_attr_reload=ship_attr_reload,
 		ship_attr_lighting=ship_attr_lighting,
 		ship_attr_motor=ship_attr_motor,
 		ship_attr_aviation=ship_attr_aviation,
@@ -181,7 +194,7 @@ def extract_all_with_splitor(selector, soup, splitor=",", attr=None):
 		str_list = list()
 		match_elements = soup.select(selector)
 		if match_elements:
-			if attr:
+			if not attr:
 				for element in match_elements:
 					str_list.append(element.get_text())
 			else:
